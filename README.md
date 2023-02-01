d3706da8b9649c45349ee159bda4c2656b50d63c
Jonah-B-Kehillah <jbarnett2023@kehillahstudent.org>
2023-01-31 19:38:51 -0800

    Finished for submission pt. one (read comment on schoology_

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# MatrixVector_JB
+# Matrix Vector Product
+###### By Jonah B.
+
+Contains implementation of a dot product function and a function that multiplies a matrix with a row vector.
+
+## Classes
+- `src`
+  - `Main`
+    - `void main(String[])`
+  - `MatrixMagic`
+    - `Double dotProduct(Double[], Double[])`
+    - `Double matrixVectorProduct(Double[][], Double[])`
+  - `TestingMatrices`
+    - `// Contains matrices to be tested in main(), to keep the Main class more organized`
+
+## Acknowledgements
+- Mr. Christopher Kuszmaul
--- src/src/MatrixMagic.java
+++ src/src/MatrixMagic.java
@@ -1,5 +1,10 @@
 public class MatrixMagic {
-
+	
+	/**
+	 * Takes the dot product of two vectors
+	 * @param a The first vector to be multiplied
+	 * @param b The second vector to be multiplied
+	 */
 	public static Double dotProduct(Double[] a, Double[] b) {
 		if(a.length < 1) throw new RuntimeException("Vectors must matrix non-zero size");
 		else if(a.length != b.length) throw new RuntimeException("Vectors must be the same size");
@@ -12,6 +17,12 @@ public class MatrixMagic {
 		return (double) Math.round(out*1000)/1000;
 	}
 	
+	/**
+	 * Takes the product of a matrix and a row vector
+	 * @param matrix The matrix to be multiplied
+	 * @param vector The (row) vector to be multiplied
+	 * @return The product of {@code matrix} and {@code vector}
+	 */
 	public static Double[] matrixVectorProduct(Double[][] matrix, Double[] vector) {
 		if(matrix.length < 1) throw new RuntimeException("Vectors must matrix non-zero size");
 		else if(matrix[0].length != vector.length) throw new RuntimeException("Vectors must be the same size");
